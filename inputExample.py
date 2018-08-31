15e251155210415c3e83e1b3a5a7dcbe0ab28794
Nick <nicholas.zimmermann@gmail.com>
2018-08-30 23:27:01 -0700

    Adding an example to the repo.

---
+++ inputExample.py
@@ -0,0 +1,16 @@
+print('please give me a number');
+num1 = input();
+print ('please give me another number');
+num2 = input();
+print ('the sum of your numbers is')
+print (num1 + num2);
+print('the difference of your numbers is');
+print(num1 - num2);
+print('the product of your numbers is ' );
+print(num1 * num2);
+if num2 == 0:
+    print('cannot divide by zero!');
+else:
+    print ('the quotient of your numbers is')
+    print (num1 / num2)
+print('that is all');
